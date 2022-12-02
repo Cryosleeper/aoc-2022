fe33a05eb4b69d874c49748ce73d4df224197b11
Cryosleeper <cryosleeper@users.noreply.github.com>
2022-12-01 22:25:41 -0500

    Day 1 part 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,6 +1,18 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var max = 0
+        var current = 0
+        for (value in input) {
+            if (value.isEmpty()) {
+                if (max < current) {
+                    max = current
+                }
+                current = 0
+            } else {
+                current += value.toInt()
+            }
+        }
+        return max
     }
 
     fun part2(input: List<String>): Int {
@@ -8,8 +20,8 @@ fun main() {
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    //val testInput = readInput("Day01_test")
+    //check(part1(testInput) == 1)
 
     val input = readInput("Day01")
     println(part1(input))
