e9cb438f77c0b7ff6645c9dcebccf175ba3cdc76
Cryosleeper <cryosleeper@users.noreply.github.com>
2022-12-04 13:18:42 -0500

    Day 4 part 2

--- src/Day04.kt
+++ src/Day04.kt
@@ -4,11 +4,24 @@ fun main() {
         return first().toInt()..last().toInt()
     }
 
-    fun IntRange.hasFullOverlap(second: IntRange): Boolean {
-        if (this.first <= second.first && this.last >= second.last) {
+    fun IntRange.hasFullOverlap(anotherRange: IntRange): Boolean {
+        if (this.first <= anotherRange.first && this.last >= anotherRange.last) {
             return true
         }
-        if (this.first >= second.first && this.last <= second.last) {
+        if (this.first >= anotherRange.first && this.last <= anotherRange.last) {
+            return true
+        }
+        return false
+    }
+
+    fun IntRange.hasAnyOverlap(anotherRange: IntRange): Boolean {
+        if (this.first <= anotherRange.first && this.last >= anotherRange.first) {
+            return true
+        }
+        if (this.first <= anotherRange.last && this.last >= anotherRange.last) {
+            return true
+        }
+        if (hasFullOverlap(anotherRange)) {
             return true
         }
         return false
@@ -26,15 +39,22 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-        TODO()
+        var count = 0
+        for (line in input) {
+            val (first, second) = line.split(",").map { it.split("-").toRange() }
+            if (first.hasAnyOverlap(second)) {
+                count++
+            }
+        }
+        return count
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day04_test")
     check(part1(testInput) == 2)
-    //check(part2(testInput) == 0)
+    check(part2(testInput) == 4)
 
     val input = readInput("Day04")
     println(part1(input))
-    //println(part2(input))
+    println(part2(input))
 }
