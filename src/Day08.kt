392a4c281c0279b699b587354a5a6544389a8237
Cryosleeper <cryosleeper@users.noreply.github.com>
2022-12-08 23:31:24 -0500

    Day 8 part 2

--- src/Day08.kt
+++ src/Day08.kt
@@ -1,3 +1,6 @@
+import kotlin.math.max
+import kotlin.math.min
+
 fun main() {
     fun part1(input: List<String>): Int {
         val numbers = input.toMatrix()
@@ -5,17 +8,18 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-        TODO()
+        val numbers = input.toMatrix()
+        return countScenic(numbers)
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day08_test")
     check(part1(testInput) == 21)
-    //check(part2(testInput) == 0)
+    check(part2(testInput) == 8)
 
     val input = readInput("Day08")
     println(part1(input))
-    //println(part2(input))
+    println(part2(input))
 }
 
 private fun List<String>.toMatrix(): List<List<Int>> = this.map { it.toList().map { it.digitToInt() } }
@@ -33,4 +37,30 @@ private fun countTrees(input: List<List<Int>>): Int {
     }
 
     return result
+}
+
+private fun countScenic(input: List<List<Int>>): Int {
+
+    fun minNonZero(a: Int, b: Int): Int {
+        return if (a == 0) b else if (b == 0) a else min(a, b)
+    }
+
+    var maxScenic = 0
+
+    input.forEachIndexed { lineIndex, line ->
+        line.forEachIndexed { columnIndex, tree ->
+            val left = input[lineIndex].subList(0, columnIndex).asReversed()
+            val leftCount = minNonZero(left.indexOfFirst { it >= tree } + 1, left.size)
+            val right = input[lineIndex].subList(columnIndex+1, input[lineIndex].size)
+            val rightCount = minNonZero(right.indexOfFirst { it >= tree } + 1, right.size)
+            val top = input.subList(0, lineIndex).asReversed().map { it[columnIndex] }
+            val topCount = minNonZero(top.indexOfFirst { it >= tree } + 1, top.size)
+            val bottom = input.subList(lineIndex+1, input.size).map { it[columnIndex] }
+            val bottomCount = minNonZero(bottom.indexOfFirst { it >= tree } + 1, bottom.size)
+
+            maxScenic = max(maxScenic, leftCount * rightCount * topCount * bottomCount)
+        }
+    }
+
+    return maxScenic
 }
