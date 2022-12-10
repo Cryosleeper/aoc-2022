3878cf10ab719526327284675f482efdd0b4e6f2
Cryosleeper <cryosleeper@users.noreply.github.com>
2022-12-09 21:48:11 -0500

    Day 9 part 2

--- src/Day09.kt
+++ src/Day09.kt
@@ -13,46 +13,41 @@ fun main() {
                 Step.LEFT -> head.x--
                 Step.RIGHT -> head.x++
             }
-            if (tail.rangeTo(head) > 2) {
-                if (tail.x > head.x && tail.y > head.y) {
-                    tail.x--
-                    tail.y--
-                } else if (tail.x > head.x && tail.y < head.y) {
-                    tail.x--
-                    tail.y++
-                } else if (tail.x < head.x && tail.y > head.y) {
-                    tail.x++
-                    tail.y--
-                } else if (tail.x < head.x && tail.y < head.y) {
-                    tail.x++
-                    tail.y++
-                }
-            } else if (tail.x - head.x == 2) {
-                tail.x--
-            } else if (tail.x - head.x == -2) {
-                tail.x++
-            } else if (tail.y - head.y == 2) {
-                tail.y--
-            } else if (tail.y - head.y == -2) {
-                tail.y++
-            }
+            tail.moveTowards(head)
             visitedPlaces.add(tail.copy())
         }
         return visitedPlaces.size
     }
 
     fun part2(input: List<String>): Int {
-        TODO()
+        val steps = input.toSteps()
+        val visitedPlaces = mutableSetOf<Position>()
+        val knots = (0 until 10).map { Position(10000, 10000) }
+        steps.forEach { step ->
+            when (step) {
+                Step.UP -> knots.first().y--
+                Step.DOWN -> knots.first().y++
+                Step.LEFT -> knots.first().x--
+                Step.RIGHT -> knots.first().x++
+            }
+            knots.forEachIndexed { index, position ->
+                if (index > 0) {
+                    position.moveTowards(knots[index-1])
+                }
+            }
+            visitedPlaces.add(knots.last().copy())
+        }
+        return visitedPlaces.size
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day09_test")
     check(part1(testInput) == 13)
-    //check(part2(testInput) == 0)
+    check(part2(testInput) == 1)
 
     val input = readInput("Day09")
     println(part1(input))
-    //println(part2(input))
+    println(part2(input))
 }
 
 private fun List<String>.toSteps(): List<Step> {
@@ -83,5 +78,31 @@ private enum class Step {
 private data class Position(var x: Int, var y: Int) {
     fun rangeTo(position: Position): Int {
         return abs(x - position.x) + abs(y - position.y)
+    }
+
+    fun moveTowards(position: Position) {
+        if (rangeTo(position) > 2) {
+            if (x > position.x && y > position.y) {
+                x--
+                y--
+            } else if (x > position.x && y < position.y) {
+                x--
+                y++
+            } else if (x < position.x && y > position.y) {
+                x++
+                y--
+            } else if (x < position.x && y < position.y) {
+                x++
+                y++
+            }
+        } else if (x - position.x == 2) {
+            x--
+        } else if (x - position.x == -2) {
+            x++
+        } else if (y - position.y == 2) {
+            y--
+        } else if (y - position.y == -2) {
+            y++
+        }
     }
 }
