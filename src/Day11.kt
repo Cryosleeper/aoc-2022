4975198eb90aed583f0b21b603d05d89842c1052
Cryosleeper <cryosleeper@users.noreply.github.com>
2022-12-11 16:46:22 -0500

    Day 11 part 2

--- src/Day11.kt
+++ src/Day11.kt
@@ -11,22 +11,31 @@ fun main() {
         return monkeys.values.map { it.thrownItems }.sortedByDescending { it }.run { get(0) * get(1) }
     }
 
-    fun part2(input: List<String>): Int {
-        TODO()
+    fun part2(input: List<String>): Long {
+        WorryModifierVeryWorried.divider = 1
+        val monkeys: Map<String, Monkey> = input.chunked(7) {
+            val monkey = Monkey(it, false)
+            monkey.id to monkey
+        }.toMap()
+        repeat(10000) {
+            println("### ROUND $it BEGINS! ###\n")
+            monkeys.forEach { it.value.inspectAndThrowToOtherMonkeys(monkeys) }
+        }
+        return monkeys.values.map { it.thrownItems }.sortedByDescending { it }.run { get(0).toLong() * get(1).toLong() }
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day11_test")
     check(part1(testInput) == 10605)
-    //check(part2(testInput) == 0)
+    check(part2(testInput) == 2713310158L)
 
     val input = readInput("Day11")
     println(part1(input))
-    //println(part2(input))
+    println(part2(input))
 }
 
 
-private class Monkey(input: List<String>) {
+private class Monkey(input: List<String>, unworry: Boolean = true) {
 
     val id: String
     private var _thrownItems = 0
@@ -38,28 +47,35 @@ private class Monkey(input: List<String>) {
 
     init {
         id = "\\d+".toRegex().find(input[0])!!.value
-        items = "\\d+".toRegex().findAll(input[1]).mapIndexed { index, matchResult -> Item("$id $index", matchResult.value.toInt()) }.toMutableList()
+        items = "\\d+".toRegex().findAll(input[1])
+            .mapIndexed { index, matchResult -> Item("$id $index", matchResult.value.toLong()) }.toMutableList()
 
         val (first, second, operator) = readWorryModifiers(input[2])
-        worryModifier = WorryModifier(first, second, operator)
 
         val divider = "\\d+".toRegex().find(input[3])!!.value.toInt()
         val trueMonkey = "\\d+".toRegex().find(input[4])!!.value
         val falseMonkey = "\\d+".toRegex().find(input[5])!!.value
         strategy = {
-            if (it.worryLevel % divider == 0) trueMonkey else falseMonkey
+            if (it.worryLevel % divider == 0L) trueMonkey else falseMonkey
+        }
+
+        worryModifier = if (unworry) {
+            WorryModifierUnworried(first, second, operator)
+        } else {
+            WorryModifierVeryWorried.divider *= divider
+            WorryModifierVeryWorried(first, second, operator)
         }
     }
 
-    private fun readWorryModifiers(input: String): Triple<Int?, Int?, WorryOperator> {
+    private fun readWorryModifiers(input: String): Triple<Long?, Long?, WorryOperator> {
         val worryModifications = input.substring(input.indexOf("=") + 2).split(" ")
-        val first: Int? = when (worryModifications[0]) {
+        val first: Long? = when (worryModifications[0]) {
             "old" -> null
-            else -> worryModifications[0].toInt()
+            else -> worryModifications[0].toLong()
         }
-        val second: Int? = when (worryModifications[2]) {
+        val second: Long? = when (worryModifications[2]) {
             "old" -> null
-            else -> worryModifications[2].toInt()
+            else -> worryModifications[2].toLong()
         }
         val operator = WorryOperator.from(worryModifications[1])
         return Triple(first, second, operator)
@@ -71,7 +87,9 @@ private class Monkey(input: List<String>) {
             println("\tMonkey $id is inspecting item ${item.id} with worry level ${item.worryLevel}")
             item.worryLevel = worryModifier.modify(item.worryLevel)
             println("\t\tItem ${item.id} has worry level ${item.worryLevel} now")
-            monkeys[strategy(item).also { println("\t\tMonkey $id throws item ${item.id} to monkey $it") }]!!.receiveItem(item)
+            monkeys[strategy(item).also { println("\t\tMonkey $id throws item ${item.id} to monkey $it") }]!!.receiveItem(
+                item
+            )
             _thrownItems++
         }
         println("\tMonkey $id has thrown $thrownItems items")
@@ -84,23 +102,46 @@ private class Monkey(input: List<String>) {
     }
 }
 
-private class WorryModifier (private val first: Int?, private val second: Int?, private val operator: WorryOperator) {
-    fun modify(currentWorry: Int): Int {
-        return (operator.perform(first ?: currentWorry, second ?: currentWorry)) / 3
+private interface WorryModifier {
+    fun modify(currentWorry: Long): Long
+}
+
+private class WorryModifierUnworried(
+    private val first: Long?,
+    private val second: Long?,
+    private val operator: WorryOperator
+) : WorryModifier {
+    override fun modify(currentWorry: Long): Long {
+        return (operator.perform(first ?: currentWorry, second ?: currentWorry)) / 3L
+    }
+}
+
+private class WorryModifierVeryWorried(
+    private val first: Long?,
+    private val second: Long?,
+    private val operator: WorryOperator
+) : WorryModifier {
+    override fun modify(currentWorry: Long): Long {
+        return (operator.perform(first ?: currentWorry, second ?: currentWorry)) % divider
+    }
+
+    companion object {
+        var divider: Long = 1
     }
 }
 
 private enum class WorryOperator {
     Add {
-        override fun perform(first: Int, second: Int) = first + second
-    }, Multiply {
-        override fun perform(first: Int, second: Int) = first * second
+        override fun perform(first: Long, second: Long) = first + second
+    },
+    Multiply {
+        override fun perform(first: Long, second: Long) = first * second
     };
 
-    abstract fun perform(first: Int, second: Int): Int
+    abstract fun perform(first: Long, second: Long): Long
 
     companion object {
-        fun from(value: String) = when(value) {
+        fun from(value: String) = when (value) {
             "+" -> Add
             "*" -> Multiply
             else -> throw IllegalArgumentException("Unknown operator: $value")
@@ -108,4 +149,4 @@ private enum class WorryOperator {
     }
 }
 
-private data class Item(val id: String, var worryLevel: Int)
+private data class Item(val id: String, var worryLevel: Long)
