74ad4f3c1694e28db30351e5d026b2551f1ea20b
Cryosleeper <cryosleeper@users.noreply.github.com>
2022-12-13 22:16:07 -0500

    Day 13 part 2

--- src/Day13.kt
+++ src/Day13.kt
@@ -5,77 +5,29 @@ fun main() {
         val pairs = input.parse()
         val indexes = mutableListOf<Int>()
         pairs.forEachIndexed { pairIndex, pair ->
-            println("Pair ${pair.first.joinToString("")} - \n     ${pair.second.joinToString("")}")
-            run pair@{
-                (0 until max(pair.first.size, pair.second.size)).forEach { index ->
-                    print("\tPosition $index: ${pair.first[index]}-${pair.second[index]}:")
-                    if (pair.first[index] == '[' && pair.second[index] == '[') {
-                        println("Starting arrays in both")
-                    } else if (pair.first[index] == ']' && pair.second[index] == ']') {
-                        println("Closing arrays in both")
-                    } else if (pair.first[index] == '[' && pair.second[index].isElevenBasedDigit()) {
-                        println("Left one starts an array, creating array in the right one")
-                        pair.second.add(index + 1, ']')
-                        pair.second.add(index, '[')
-                        println("\t\tNow comparing ${pair.first.joinToString("")} - \n" +
-                                "\t\t              ${pair.second.joinToString("")}")
-                    } else if (pair.first[index].isElevenBasedDigit() && pair.second[index] == '[') {
-                        println("Right one starts an array, creating array in the left one")
-                        pair.first.add(index + 1, ']')
-                        pair.first.add(index, '[')
-                        println("\t\tNow comparing ${pair.first.joinToString("")} - \n" +
-                                "\t\t              ${pair.second.joinToString("")}")
-                    } else if (pair.first[index] == ']' && pair.second[index] != ']') {
-                        println("Left one closes an array, sequence is correct")
-                        indexes.add(pairIndex)
-                        return@pair
-                    } else if (pair.first[index] != ']' && pair.second[index] == ']') {
-                        println("Right one closes an array, sequence is wrong")
-                        return@pair
-                    } else if (pair.first[index].isElevenBasedDigit() && pair.second[index].isElevenBasedDigit()) {
-                        println("Comparing ${pair.first[index]} and ${pair.second[index]}")
-                        val first = pair.first[index] - '0'
-                        val second = pair.second[index] - '0'
-                        if (first < second) {
-                            println("\tCorrect sequence")
-                            indexes.add(pairIndex)
-                            return@pair
-                        } else if (first > second) {
-                            println("\tWrong sequence")
-                            return@pair
-                        } else {
-                            println("\tSame digit, continuing")
-                        }
-                    } else {
-                        println("Skipping coma")
-                    }
-                    if (index == pair.first.size - 1) {
-                        println("\tLeft one is shorter, sequence is correct")
-                        indexes.add(pairIndex)
-                        return@pair
-                    } else if (index == pair.second.size - 1) {
-                        println("\tRight one is shorter, sequence is wrong")
-                        return@pair
-                    }
-                }
-            }
+            if (comparePair(pair)) indexes.add(pairIndex)
         }
 
         return indexes.sumOf { it+1 }
     }
 
     fun part2(input: List<String>): Int {
-        TODO()
+        val parsed = input.filter { it.isNotEmpty() }.map { it.replace("10", ":").toMutableList() }.toMutableList()
+        parsed.add("[[2]]".toMutableList())
+        parsed.add("[[6]]".toMutableList())
+        val sorted = parsed.sortedWith { o1, o2 -> if (comparePair(o1 to o2)) -1 else 1 }
+        println("Sorted list is ${sorted.map { it.joinToString("", postfix = "\n") }}")
+        return (sorted.indexOf("[[2]]".toList())+1) * (sorted.indexOf("[[6]]".toList())+1)
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day13_test")
     check(part1(testInput) == 13)
-    //check(part2(testInput) == 0)
+    check(part2(testInput) == 140)
 
     val input = readInput("Day13")
     println(part1(input))
-    //println(part2(input))
+    println(part2(input))
 }
 
 private fun List<String>.parse(): List<Pair<MutableList<Char>, MutableList<Char>>> {
@@ -87,3 +39,70 @@ private fun List<String>.parse(): List<Pair<MutableList<Char>, MutableList<Char>
 }
 
 private fun Char.isElevenBasedDigit() = this-'0' in 0..10
+
+fun comparePair(
+    pair: Pair<MutableList<Char>, MutableList<Char>>
+): Boolean {
+    var result = false
+    println("Pair ${pair.first.joinToString("")} - \n     ${pair.second.joinToString("")}")
+    run pair@{
+        val firstCode = mutableListOf<Char>().apply { addAll(pair.first) }
+        val secondCode = mutableListOf<Char>().apply { addAll(pair.second) }
+        (0 until max(firstCode.size, secondCode.size)).forEach { index ->
+            print("\tPosition $index: ${firstCode[index]}-${secondCode[index]}:")
+            if (firstCode[index] == '[' && secondCode[index] == '[') {
+                println("Starting arrays in both")
+            } else if (firstCode[index] == ']' && secondCode[index] == ']') {
+                println("Closing arrays in both")
+            } else if (firstCode[index] == '[' && secondCode[index].isElevenBasedDigit()) {
+                println("Left one starts an array, creating array in the right one")
+                secondCode.add(index + 1, ']')
+                secondCode.add(index, '[')
+                println(
+                    "\t\tNow comparing ${firstCode.joinToString("")} - \n" +
+                            "\t\t              ${secondCode.joinToString("")}"
+                )
+            } else if (firstCode[index].isElevenBasedDigit() && secondCode[index] == '[') {
+                println("Right one starts an array, creating array in the left one")
+                firstCode.add(index + 1, ']')
+                firstCode.add(index, '[')
+                println(
+                    "\t\tNow comparing ${firstCode.joinToString("")} - \n" +
+                            "\t\t              ${secondCode.joinToString("")}"
+                )
+            } else if (firstCode[index] == ']' && secondCode[index] != ']') {
+                println("Left one closes an array, sequence is correct")
+                result = true
+                return@pair
+            } else if (firstCode[index] != ']' && secondCode[index] == ']') {
+                println("Right one closes an array, sequence is wrong")
+                return@pair
+            } else if (firstCode[index].isElevenBasedDigit() && secondCode[index].isElevenBasedDigit()) {
+                println("Comparing ${firstCode[index]} and ${secondCode[index]}")
+                val first = firstCode[index] - '0'
+                val second = secondCode[index] - '0'
+                if (first < second) {
+                    println("\tCorrect sequence")
+                    result = true
+                    return@pair
+                } else if (first > second) {
+                    println("\tWrong sequence")
+                    return@pair
+                } else {
+                    println("\tSame digit, continuing")
+                }
+            } else {
+                println("Skipping coma")
+            }
+            if (index == firstCode.size - 1) {
+                println("\tLeft one is shorter, sequence is correct")
+                result = true
+                return@pair
+            } else if (index == secondCode.size - 1) {
+                println("\tRight one is shorter, sequence is wrong")
+                return@pair
+            }
+        }
+    }
+    return result
+}
